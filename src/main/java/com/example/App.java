45204ef5a54f453a5636975a210925e4d88f2293
sowmya <sowmyasomu0772gmail.com>
2026-06-20 12:54:51 +0530

    first commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,19 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello maven" );
+        System.out.println("this is the simple real world example");
+
+        int a=10;
+        int b=5;
+        int sum=a+b;
+        System.out.println("sum="+sum);
+    }
+}
